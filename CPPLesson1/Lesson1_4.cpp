86bbe87725f856607b67be4c6c05192c667ca735
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-02-07 23:15:13 +0500

    Add files via upload

---
+++ CPPLesson1/Lesson1_4.cpp
@@ -0,0 +1,43 @@
+﻿// Операторы.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	system("cls");          // Очистка экрана
+	int goldInChest = 10;  
+	
+	cout << "Хо-хо-хо! У меня в сундучке " << goldInChest << " золотых монет!" << endl;
+	
+	// + - * / %
+	goldInChest = goldInChest + 2;	// Эти действия равнозначны
+	goldInChest += 2;				// Эти действия равнозначны
+	
+	goldInChest += 1;
+	cout << "Хо-хо-хо! У меня в сундучке " << goldInChest << " золотых монет!" << endl; //15
+	
+	// Инкремент и Декремент
+	//goldInChest++; // +1
+	cout << "Хо-хо-хо! А теперь У меня в сундучке " << goldInChest++ << " золотых монет!" << endl;
+	//++goldInChest; // +1
+	
+	cout << "Хо-хо-хо! Больше Золота! У меня в сундучке " << goldInChest << " золотых монет!" << endl;
+
+	// Преобразование типов
+	//bool, char, short - int - unsigned int - 
+		//long - unsigned long - float - double - long double
+	int gold = 25.5; // сужающее преобразование не явное
+	
+	unsigned int gold2 = 25; // расширяющее преобразование не явное
+
+	float money = 10.25; // double стал float сужающее преобразование не явное
+
+	double goldInSafe = 1000.01;
+	money = (int)goldInSafe; //явное сужающее
+	cout << "Грабим сейф! Теперь У меня в кармане " << money << " золотых монет!" << endl;
+
+}
+
---
+++ CPPLesson1/Lesson1_5.cpp
@@ -0,0 +1,53 @@
+﻿// Логические Операции
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	system("cls");          // Очистка экрана
+
+	// Операторы сравнения
+	//  < > <= >=
+	cout << (5 < 3) << endl; // 0
+	cout << (5 > 3) << endl; // 1
+	
+	cout << boolalpha << (5 < 3) << endl; // false
+	cout << boolalpha << (5 > 3) << endl; // true 
+	
+	// Операторы равенства
+	// == !=
+	cout << boolalpha << (5 != 3) << endl; // true
+	
+	// Логические Объединения и отрицательная инверсия
+	//  И &&      ИЛИ ||      НЕ !
+	cout << boolalpha << ((5 > 3) && (6 == 3)) << endl; // флаг boolalpha
+	
+	// конъюнкция, логическое умножение
+	//Утверждение 1 Утверждение 2    &&
+	//	true		true			true      1 1 1
+	//	true		false			false     1 0 0
+	//	false		true			false
+	//	false		false			false
+
+	// дизъюнкция, логическое сложение
+	//Утверждение 1 Утверждение 2    ||
+	//	true		true			true     1 1 1
+	//	true		false			true	 1 0 1
+	//	false		true			true
+	//	false		false			false    0 0 0
+
+	int num;
+	cout << "Введите число от 1 до 10: ";
+	cin >> num;
+	cout << boolalpha << ((num >= 1) && (num <= 10)) << endl;
+	cout << "Если вы видите true, то вы ввели правильное число.\n";
+	cout << "Если вы видите false, то вы ввели число в другом диапазоне.\n";
+
+	//num = 0; !num  --- истина
+
+
+}
