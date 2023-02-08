b867146a16e214969d1dadef8bcf0bc1a5255c51
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-02-08 21:25:29 +0500

    Add files via upload

---
+++ 08_02_23_1 (задачи).cpp
@@ -0,0 +1,53 @@
+﻿// 08_02_23_1 (задачи)
+//
+//найти индексы первого вхождения (ключ) массива размерами не более чем 100х100
+
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+
+    int row, column;//строка, столбец массива
+    cout << "Программа для максимального вхождениия максимального элемента массива v1.0\n";
+    cout << "Введите кол-во строк и столбцов:\n";
+    cin >> row >> column;
+    int array[100][100]; //ограничение размеров массива 100х100
+   
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array [i][j] = 1 + rand() % 99;//наполняем массив рандомными числами
+            cout << array[i][j]<<"\t";
+        }
+        cout << "\n";
+    }
+    int arrRes = array[0][0], r_max = 0, k_max = 0; //второй массив для одновременного вывода результатов
+    for (int r = 0; r < row; r++)
+    {
+        for (int k = 0; k < column; k++)
+        {
+            if (array[r][k]>arrRes)
+            {
+                arrRes = array[r][k];
+                r_max = r;
+                k_max = k;
+
+            }
+        }
+    }
+
+    cout << r_max << " " << k_max;
+
+
+
+    system("pause");
+    return 0;
+}
+
---
+++ 08_02_23_2 (задача на массивы).cpp
@@ -0,0 +1,50 @@
+﻿// 08_02_23_2 (задачи)
+//
+//создать массив из символов
+
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+
+    int sizeArr = 0;
+    //ограничение размера массива (незаполненные (меньшие размеры) заполняются нулями, если больше - вылет программы)
+    int arrStar[15][15]; // проверка на валидность вводных данных от пользователя (3 сита: правды, истинности и т.д.)
+    //рисовать будем не массивом а cout
+
+
+    cout << "Программа нарисуй звёздочку версия 2.0" << "\n";
+    cout << "Введите нечетное число, не больше 15" << "\n";
+    cin >> sizeArr;
+    for (int i = 0; i < sizeArr; i++)
+    {
+        for (int j = 0; j < sizeArr; j++)
+        {
+            //заполняем массив "маркерами"
+            if (i == j || i == sizeArr - 1 - j || i == sizeArr / 2 || j == sizeArr / 2) arrStar[i][j] = 1;
+            else arrStar[i][j] = 0;
+            cout << arrStar[i][j];
+        }
+
+        cout << "\n";
+    }
+    for (int r = 0; r < sizeArr; r++)
+    {
+        for (int k = 0; k < sizeArr; k++)
+        {
+            if (arrStar[r][k] == 0) cout << "." << " ";
+            else cout << "*" << " ";
+        }
+        cout << "\n";
+    }
+
+
+    system("pause");
+    return 0;
+}
