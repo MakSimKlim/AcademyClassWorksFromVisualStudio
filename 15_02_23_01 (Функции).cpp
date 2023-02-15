c74c5713d9ea924421f12a152fdee08f8e433ba7
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-02-15 21:40:04 +0500

    Add files via upload

---
+++ 15_02_23_01 (Функции).cpp
@@ -0,0 +1,62 @@
+﻿// 15_02_23_01 (Функции)
+//
+//
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void loadArr(int arr[], int sizeArr)//результат из функции выйдет измененный массив (это работает только с массивами, с int так не работает)
+{
+	for (int i = 0; i < sizeArr; i++)
+	{
+		arr[i] = 1 + rand() % 10;
+	}
+}
+
+//int loadArr(int arr[], int sizeArr)// вариант замены функции VOID
+//{
+//	const int sArr = 5;
+//	int returnArr[sArr];
+//	
+//	
+//	
+//	for (int i = 0; i < sizeArr; i++)
+//	{
+//		arr[i] = 1 + rand() % 10;
+//		returnArr[i] = arr[i];
+//		cout << returnArr[i] << " ";
+//	}
+//	return returnArr[sArr];
+//}
+
+
+void printArr(int arr[], int sizeArr)
+{
+	for (int i = 0; i < sizeArr; i++)
+	{
+		cout << arr[i]<<" ";
+	}
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+	const int sizeA = 5;
+	int arrayTest[sizeA];
+	
+	//
+	//for (int i = 0; i < sizeA; i++)
+	//{
+	//	cout << arrayTest[i]<<" ";
+	//}
+	
+	loadArr(arrayTest, sizeA);
+
+	printArr(arrayTest, sizeA);
+
+}
