41b6ea652351d889049277f6534f38a48fd2b068
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-02-17 21:38:37 +0500

    Add files via upload

---
+++ 17_02_23_02 (Рекурсия задачи).cpp
@@ -0,0 +1,26 @@
+﻿// 17_02_23_02 (Рекурсия задачи)
+//
+//Написать функцию для вычисления факториала
+//
+//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int fact(int n)//нач знач, массив, размер знач
+{
+    if (n==0)
+        return 1;
+    else
+        return fact(n-1)*n;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+    int n = 5;
+    cout << fact(n);
+
+}
