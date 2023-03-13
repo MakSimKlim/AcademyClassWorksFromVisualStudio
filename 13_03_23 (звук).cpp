719bf18fc8543514b0a771ab7d44da953b23cf71
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-03-13 21:43:17 +0500

    Add files via upload

---
+++ 13_03_23 (звук).cpp
@@ -0,0 +1,41 @@
+﻿// 13_03_23 (звук)
+#include <iostream> 
+#include <windows.h> // WinApi header 
+
+using namespace std;
+
+int main()
+{
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(329, 500);
+    Beep(247, 500);
+    Beep(247, 500);
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(497, 500);
+    Sleep(500);
+    Beep(497, 500);
+    Beep(277, 500);
+    Beep(277, 500);
+    Beep(440, 500);
+    Beep(440, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(329, 500);
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(329, 500);
+
+    cin.get(); // wait 
+    return 0;
+}
