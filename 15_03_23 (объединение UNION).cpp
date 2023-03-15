f2d892e1b4c4a3d7fe8ff8811691ba2ad433c62b
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-03-15 21:26:30 +0500

    Add files via upload

---
+++ 15_03_23 (объединение UNION).cpp
@@ -0,0 +1,35 @@
+﻿// 15_03_23 (объединение - UNION)
+//
+//
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+union Employee
+{
+	int id;
+	double salary;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	Employee myEmployee;
+
+	myEmployee.id = 15;
+	cout << "The first value (id) at ";
+	cout << "the allocated memory: ";
+	cout << myEmployee.id << "\n";
+	myEmployee.salary = 100.35;//затирает предыдущее id
+	cout << "The next value (salary) stored";
+	cout << "after removing the previous value: ";
+	cout << myEmployee.salary << "\n";
+	myEmployee.id = 30;//затирает предыдущее salary
+	cout << "The first value (id) at ";
+	cout << "the allocated memory: ";
+	cout << myEmployee.id << "\n";
+
+	return 0;
+}
