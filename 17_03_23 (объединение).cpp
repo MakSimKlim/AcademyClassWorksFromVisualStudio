4bbd7fc53123df91332d70c3e3ee54b2595c86ad
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-03-17 21:31:40 +0500

    Add files via upload

---
+++ 17_03_23 (объединение).cpp
@@ -0,0 +1,35 @@
+﻿// 17_03_23 (объединение)
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+struct Test                        // структура это тип данных!!!
+{
+	// битовые поля
+	unsigned int Python : 1; //поля будут строго занимать 3 бита (:1)
+	unsigned int JS : 3;//поле будет занимать 3 бита (:3)
+	unsigned int PHP : 8; //поле будет занимать 8 бит (:3)
+
+	// 0...7 (3 бит)
+	// 0...31 (6 бит)
+	// 0...12 (5 бит)
+	// 0...100 (8 бит)
+};
+
+union Employee // объединение
+{
+	int id;
+	double salary;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	
+	cout << sizeof Test << "\n";// показывает размер структуры
+	cout << sizeof Employee << "\n"; // показывает размер объединенеия
+
+	return 0;
+}
---
+++ 17_03_23 (работа с файлами2).cpp
@@ -0,0 +1,27 @@
+﻿// 17_03_23 (работа с файлами2)
+//
+//
+#include <iostream>
+#include <time.h>
+#include <fstream> // эта библиотека лучше, чем cstdio
+
+using namespace std;
+
+// открываем файл
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	ofstream out;
+
+	out.open("D:/Климов/мои проги/!AcademyWorks/TestFileForOpenInCPP.txt");
+
+	if (out.is_open())
+	{
+		out << "я перезаписал файл2" << "\n";
+	}
+
+	cout << "End of program" << "\n";
+
+	return 0;
+}
