9dc1847e34fef2862d74121ddbbfaae9446c2744
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-10 21:46:55 +0500

    Add files via upload

--- 07_04_23/07_04_23 (Классы ОБОИ).cpp
+++ 07_04_23/07_04_23 (Классы ОБОИ).cpp
@@ -19,31 +19,23 @@ int main()
 
     cout << "Расчет обоев версия 1.0" << "\n";
     int inputCountRoom = 0;
-    double roomWidth = 0, roomLength = 0, roomHeight = 0;
+    double roomWidth = 0, roomLength = 0, roomHeight = 0, result = 0;
+
+    double* res = new double[inputCountRoom];
 
     cout << "Введите количество комнат, в которых требуется сделать ремонт" << "\n";
     cin >> inputCountRoom;
 
-    Room* room = new Room[inputCountRoom];//массив с комнатами
+    Apartment apartment(inputCountRoom);
+    res = apartment.createRooms(roomWidth, roomLength, roomHeight, result);
 
+    //cout << "Площадь стен комнат:" << "\n";
     for (int i = 0; i < inputCountRoom; i++)
     {
-        cout << "Введите ширину комнаты: " << "\n";
-        cin >> roomWidth;
-        room[i].width = roomWidth;
-        cout << "Введите длину комнаты: " << "\n";
-        cin >> roomLength;
-        room[i].length = roomLength;
-        cout << "Введите высоту комнаты: " << "\n";
-        cin >> roomHeight;
-        room[i].height = roomHeight;
-
-        double result = room[i].getSquare();
-
-        cout << "Площадь стен в комнате #"<< i+1 << " равна: " << result << "\n";
-
+       cout<<"Площадь стен комнаты №"<< i+1 <<" равна " << res[i] << " м2" << "\n";
     }
 
+    //double paramsWallpaper[7][3] = {};
 
 }
 
--- 07_04_23/Source.cpp
+++ 07_04_23/Source.cpp
@@ -23,46 +23,83 @@ public:
     Apartment(int apartCount)
     {
         countRoom = apartCount;
-    }
+    };
 
-    void getCountRoom(int* rooms, int counter)
+    //комната 
+    class Room
     {
-        for (int i = 0; i < counter; i++)
+    public:
+        double width, length, height;
+
+        //конструктор, здесь нельз€ делать какие-то вычислени€
+        //т к здесь объ€вл€ем свойства присущие объекту
+        //конструктор ничего не возвращает
+        Room(double c_width, double c_height, double c_length)
         {
-            cout << rooms[i] << "\n";
+            width = c_width;
+            length = c_length;
+            height = c_height;
+        };
+
+        //метод 
+        double getSquare()
+        {
+            return ((width + length) * 2 * height);
         }
-    }
-};
 
-//комната 
-class Room
-{
-public:
-    double width, height, length;
+        //конструктор пустой дл€ создани€ нескольких объектов, чтобы не 
+        //перегружать пам€ть? без него работать не будет
+        Room()
+        {
 
-    //конструктор, здесь нельз€ делать какие-то вычислени€
-    //т к здесь объ€вл€ем свойства присущие объекту
-    //конструктор ничего не возвращает
-    Room(double c_width, double c_height, double c_length)
-    {
-        width = c_width;
-        height = c_height;
-        length = c_length;
+        }
+
+    private:
 
     };
 
-    //метод 
-    double getSquare()
+    double *createRooms(double roomWidth, double roomLenght, double roomHeight, double result)
     {
-        return ((width+ length)*2* length);
+        int inputCountRoom = countRoom;
+        Room* room = new Room[inputCountRoom];
+        double* rooms = new double[countRoom];
+        for (int i = 0; i < inputCountRoom; i++)
+        {
+            cout << "¬ведите ширину комнаты: " << "\n";
+            cin >> roomWidth;
+            room[i].width = roomWidth;
+            cout << "¬ведите длину комнаты: " << "\n";
+            cin >> roomLenght;
+            room[i].length = roomLenght;
+            cout << "¬ведите высоту комнаты: " << "\n";
+            cin >> roomHeight;
+            room[i].height = roomHeight;
+
+            rooms[i] = room[i].getSquare();
+        }
+       return rooms;
     }
+};
 
-    Room()
+class Wallpaper
+{
+public:
+
+    //конструктор дл€ рулона обоев
+    Wallpaper(double setWidth, double setLength, double setPrice, string setName)
     {
-
+        rollWidth = setWidth;
+        rollLength = setLength;
+        rollPrice = setPrice;
+        rollName = setName;
     }
 
 private:
+    double rollWidth, rollLength, rollPrice;
+    string rollName;
+
+
 
 };
 
+      
