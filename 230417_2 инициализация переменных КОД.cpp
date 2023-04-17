4eb890fb5cdb67f1ff11e9220909f8fd38c75746
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-17 21:49:17 +0500

    Add files via upload

---
+++ 230417_2 инициализация переменных КОД.cpp
@@ -0,0 +1,53 @@
+﻿// 230417_2 инициализация переменных КОД
+// 
+// Класс человек
+// 
+// поля:
+// идентификационный номер
+// фамилия
+// имя
+// отчество
+// дата рождения
+// 
+// должен быть конструктор с параметрами
+// сеттеры- методы которые что-то получают
+// геттеры- методы которые что-то получают
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+
+		class Human
+	{
+	public:
+
+		
+
+
+		
+
+
+	private:
+		int id;
+		string* surname = new string[20]; // фамилия 
+		string* name = new string[20]; // имя 
+		string* secondName = new string[20]; // отчество
+		struct Birthday // структура даты рождения
+		{
+			int day; // день
+			int month; // месяц
+			int year; // год
+		};
+
+	};
+
+
+
+}
+
