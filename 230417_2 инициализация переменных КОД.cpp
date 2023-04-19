9ed8e88131155638070f5fa9fcc9f2345cbfb9ad
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-19 21:43:30 +0500

    Add files via upload

--- 230417_2 инициализация переменных КОД.cpp
+++ 230417_2 инициализация переменных КОД.cpp
@@ -15,39 +15,60 @@
 //
 
 #include <iostream>
+#include <cstdlib> // содержит srand() и rand()
 
 using namespace std;
 
+class Human
+{
+public:
+	//прототип функции вызова приватных переменных класса
+	void getName(int id, int age, string surname, string name, string secondName);
+
+private:
+	int id;
+	int age;
+	string surname; // фамилия 
+	string name; // имя 
+	string secondName; // отчество
+};
+
+//функция вынесенная за тело класса
+void Human::getName(int id, int age, string surname, string name, string secondName) 
+{
+	cout << " Порядковый номер: " << id << "\n";
+	cout << " Возраст человека: " << age << "\n";
+	cout << " Фамилия человека: " << surname << "\n";
+	cout << " Имя человека: " << name << "\n";
+	cout << " Отчество человека: " << secondName << "\n";
+	cout << "\n";
+}
+
+
 int main()
 {
-
 	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
 
-		class Human
+	cout << "Программа Класс 'Человек'" << "\n";
+	cout << "\n";
+
+	string surname = "Иванов ";
+	string name = "Иван ";
+	string secondName = "Иванович ";
+	const int kolichestvo = 10;
+	//инициализация объекта chelovek класса human
+	Human chelovek[kolichestvo];
+
+
+	for (int i = 0; i < kolichestvo; i++)
 	{
-	public:
+		int id = i+1;
+		int age = 10 + rand() % 90 - 10;
+		//присвоение значений объекту
+		chelovek[i].getName(id, age, surname, name, secondName);
 
-		
-
-
-		
-
-
-	private:
-		int id;
-		string* surname = new string[20]; // фамилия 
-		string* name = new string[20]; // имя 
-		string* secondName = new string[20]; // отчество
-		struct Birthday // структура даты рождения
-		{
-			int day; // день
-			int month; // месяц
-			int year; // год
-		};
-
-	};
-
-
+	}
 
 }
 
